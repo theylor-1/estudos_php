8e9074445c8be21ce22de6a028869de840227ab9
Theylor <theylor@unochapeco.edu.br>
2025-04-25 22:26:17 -0300

    Treino de condicionais

--- helpers.php
+++ helpers.php
@@ -1,17 +1,58 @@
 <?php
 
-function saudacao($hora) {
+$hora = null;
+function saudacaoIf($hora)
+{
     if ($hora >= 0 && $hora < 6) {
-        $saudacao = 'Boa madrugada'; 
-    } elseif ($hora >= 6 && $hora < 12) {
-        $saudacao = 'Bom dia';
-    } elseif ($hora >= 12 && $hora < 18) {
-        $saudacao = 'Boa tarde';
-    } else {
-        $saudacao = 'Boa noite';
+        echo 'Boa madrugada';
     }
-
-    return $saudacao;
+    if ($hora >= 6 && $hora < 12) {
+        echo 'Bom dia';
+    }
+    if ($hora >= 12 && $hora < 18) {
+        echo 'Boa tarde';
+    }
+    if ($hora >= 18 && $hora < 23) {
+        echo 'Boa noite';
+    }
 }
 
-?>
+function saudacaoIfelse($hora)
+{
+    if ($hora >= 0 && $hora < 12) {
+        echo 'Bom dia!';
+    } else {
+        echo 'Boa noite!';
+    }
+}
+
+function saudacaoElseIf($hora)
+{
+    if ($hora >= 0 && $hora < 6) {
+        echo 'madrugada';
+    } elseif ($hora >= 6 && $hora < 12) {
+        echo 'dia';
+    } elseif ($hora >= 12 && $hora < 18) {
+        echo 'tarde';
+    } elseif ($hora >= 18 && $hora < 23) {
+        echo 'noite';
+    } else {
+        echo 'hora inválida';
+    }
+}
+function saudacaoBreak($hora)
+{
+    switch ($hora) {
+        case 1:  // Manhã
+            echo 'Manhã';
+            break;
+        case 2: // Tarde
+            echo 'Tarde';
+            break;
+        case 3: // Noite
+            echo 'Noite';
+            break;
+        default:
+            echo 'Valor inválido';
+    }
+}
---
+++ loopIndex.php
@@ -0,0 +1,12 @@
+<?php
+include_once 'helpers.php';
+date_default_timezone_set('America/Sao_Paulo');
+//Testes
+$boaMadrugada = 0;
+$bomDia = 6;
+$boaTarde = 12;
+$hora = date("H");
+//saudacaoIf($boaTarde);
+//saudacaoIfelse($bomDia = -1);
+//saudacaoElseIf($boaTarde= 2444);
+saudacaoBreak($hora = 4);
